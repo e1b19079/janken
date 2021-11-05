57f769e059a0690cd9067850e85f4483782ecfdc
e1b19079 <e1b19079@oit.ac.jp>
2021-11-05 21:53:35 +0900

    ユーザ情報をSELECTで取得して表示する

---
+++ src/main/java/oit/is/z0119/kaizi/janken/model/MatchMapper.java
@@ -0,0 +1,15 @@
+package oit.is.z0119.kaizi.janken.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface MatchMapper {
+
+  @Select("SELECT * FROM MATCHES")
+  ArrayList<Match> selectAll();
+}
